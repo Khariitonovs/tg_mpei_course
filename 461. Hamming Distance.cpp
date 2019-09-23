c1b33d679bea77ba2a45a0aef9d66752fcc356fc
Khariitonovs <55653857+Khariitonovs@users.noreply.github.com>
2019-09-23 18:50:41 +0300

    KM-1

---
+++ 461. Hamming Distance.cpp
@@ -0,0 +1,17 @@
+https://leetcode.com/problems/hamming-distance/
+
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int dist = 0;
+        while (x > 0 || y > 0) {
+            if (x % 2 != y % 2) {   
+                dist++;
+            }
+			x /= 2; 
+            y /= 2;
+        }
+        return dist;
+        
+    }
+};
